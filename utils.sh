eae0ebdd9e3e44ad34e434f3cf8b97ba98db4532
joao-paulo-parity <joao-paulo@parity.io>
2022-04-25 09:50:56 -0300

    support multiple arguments for the same option

--- utils.sh
+++ utils.sh
@@ -6,41 +6,58 @@ die() {
 }
 
 get_arg() {
+  local arg_type="$1"
+  shift
+
   local is_required
-  case "$1" in
-    required)
+  case "$arg_type" in
+    required|required-multi)
       is_required=true
     ;;
-    optional)
-      is_required=false
-    ;;
+    optional|optional-multi) ;;
     *)
       die "Invalid is_required argument \"$2\" in get_arg"
     ;;
   esac
-  shift
+
+  local is_multi
+  if [ "${arg_type: -6}" == "-multi" ]; then
+    is_multi=true
+  fi
 
   local option_arg="$1"
   shift
 
   unset out
+  out=()
 
   local get_next_arg
   for arg in "$@"; do
     if [ "${get_next_arg:-}" ]; then
-      out="$arg"
-      break
+      out+=("$arg")
+      if [ ! "${is_multi:-}" ]; then
+        break
+      fi
+      unset get_next_arg
     # --foo=bar (get the value after '=')
     elif [ "${arg:0:$(( ${#option_arg} + 1 ))}" == "$option_arg=" ]; then
-      out="${arg:$(( ${#option_arg} + 1 ))}"
-      break
+      out+=("${arg:$(( ${#option_arg} + 1 ))}")
+      if [ ! "${is_multi:-}" ]; then
+        break
+      fi
     # --foo bar (get the next argument)
     elif [ "$arg" == "$option_arg" ]; then
       get_next_arg=true
     fi
   done
 
-  if [[ ! "${out:-}" && $is_required == true ]]; then
+  if [ "${out[0]:-}" ]; then
+    if [ ! "${is_multi:-}" ]; then
+      out="${out[0]}"
+    fi
+  elif [ "${is_required:-}" ]; then
     die "Argument $option_arg is required, but was not found"
+  else
+    unset out
   fi
 }
