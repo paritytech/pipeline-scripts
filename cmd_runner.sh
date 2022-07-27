31ed0d240439e776d75cfd65e788712c3fa511c4
joao-paulo-parity <joao-paulo@parity.io>
2022-07-26 23:21:49 -0300

    bench-bot: take into account commits made before the command's start

--- cmd_runner.sh
+++ cmd_runner.sh
@@ -16,9 +16,19 @@ cmd_runner_setup() {
   # set the Git user, otherwise Git commands will fail
   git config --global user.name command-bot
   git config --global user.email "<>"
+  git config --global pull.rebase false
 
   # Reset the branch to how it was on GitHub when the bot command was issued
   git reset --hard "$GH_HEAD_SHA"
+
+  # Some commands push commits to the requester's branch, therefore we should
+  # pull the branch from GitHub before running a command it so that its
+  # execution takes into account commits pushed before its start
+  git remote add \
+    github \
+    "https://token:${GITHUB_TOKEN}@github.com/${GH_CONTRIBUTOR}/${GH_CONTRIBUTOR_REPO}.git"
+  git pull --ff --no-edit github "$GH_CONTRIBUTOR_BRANCH"
+  git remote remove github
 
   cmd_runner_display_rust_toolchain
 
