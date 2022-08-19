ddf46b72aed0905e2a46b7227024ba25904ddfb5
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-08-19 14:47:16 +0100

    remove output file

--- bench-bot.sh
+++ bench-bot.sh
@@ -94,20 +94,17 @@ bench_pallet() {
       fi
       local weights_dir="./runtime/${runtime_dir}/src/weights"
 
-      # translates e.g. "pallet_foo::bar" to "pallet_foo_bar"
-      local output_file="${pallet//::/_}"
-
       case "$kind" in
         runtime)
           args+=(
             --header=./file_header.txt
-            --output="${weights_dir}/${output_file}.rs"
+            --output="${weights_dir}/"
           )
         ;;
         xcm)
           args+=(
             --template=./xcm/pallet-xcm-benchmarks/template.hbs
-            --output="${weights_dir}/xcm/${output_file}.rs"
+            --output="${weights_dir}/xcm/"
           )
         ;;
         *)
@@ -129,20 +126,17 @@ bench_pallet() {
         --header=./file_header.txt
       )
 
-      # translates e.g. "pallet_foo::bar" to "pallet_foo_bar"
-      local output_file="${pallet//::/_}"
-
       case "$kind" in
         pallet)
           args+=(
-            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/${output_file}.rs"
+            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/"
           )
         ;;
         xcm)
           mkdir -p "./parachains/runtimes/$chain_type/$runtime/src/weights/xcm"
           args+=(
             --template=./templates/xcm-bench-template.hbs
-            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/xcm/${output_file}.rs"
+            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/xcm/"
           )
         ;;
         *)
