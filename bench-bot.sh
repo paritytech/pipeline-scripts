6d3b6200167b1afe0c8501d24ab810c16ce23474
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-09-23 12:46:06 +0200

    Only use folder name for --output
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- bench-bot.sh
+++ bench-bot.sh
@@ -94,23 +94,17 @@ bench_pallet() {
       fi
       local weights_dir="./runtime/${runtime_dir}/src/weights"
 
-      local output_file=""
-      if [[ $pallet == *"::"* ]]; then
-        # translates e.g. "pallet_foo::bar" to "pallet_foo_bar"
-        output_file="${pallet//::/_}.rs"
-      fi
-
       case "$kind" in
         runtime)
           args+=(
             --header=./file_header.txt
-            --output="${weights_dir}/${output_file}"
+            --output="${weights_dir}/"
           )
         ;;
         xcm)
           args+=(
             --template=./xcm/pallet-xcm-benchmarks/template.hbs
-            --output="${weights_dir}/xcm/${output_file}"
+            --output="${weights_dir}/xcm/"
           )
         ;;
         *)
@@ -132,23 +126,17 @@ bench_pallet() {
         --header=./file_header.txt
       )
 
-      local output_file=""
-      if [[ $pallet == *"::"* ]]; then
-        # translates e.g. "pallet_foo::bar" to "pallet_foo_bar"
-        output_file="${pallet//::/_}.rs"
-      fi
-
       case "$kind" in
         pallet)
           args+=(
-            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/${output_file}"
+            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/"
           )
         ;;
         xcm)
           mkdir -p "./parachains/runtimes/$chain_type/$runtime/src/weights/xcm"
           args+=(
             --template=./templates/xcm-bench-template.hbs
-            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/xcm/${output_file}"
+            --output="./parachains/runtimes/$chain_type/$runtime/src/weights/xcm/"
           )
         ;;
         *)
