a8f1136d5bbfa4f339d1dc2d652d636492b8c299
Gerawork Aynekulu <ggera@users.noreply.github.com>
2022-11-21 17:43:20 +0100

    Update bench-bot-run.sh

--- bench-bot-run.sh
+++ bench-bot-run.sh
@@ -156,7 +156,7 @@ bench_pallet() {
       pallet)
         # We also need to translate '_' to '-' due to the folders' naming
         # conventions
-        output_dir="${output_dir//_/-}"
+        # output_dir="${output_dir//_/-}"
 
         args+=(
           --output="./pallets/$output_dir/src/default_weights.rs"
